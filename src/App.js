bc02b60f7a95978430e305315455dcc4daad1937
JashUpatel <pateljash77@gmail.com>
2020-09-09 19:48:09 +0530

    changing https api req

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ class App extends Component{
 constructor(props){
   super(props);
   this.state={
-    bitcoin:"http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-09&sortBy=publishedAt&apiKey=01ff551c8604416ca22e94875f6265f1",
-    busines:"http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=01ff551c8604416ca22e94875f6265f1",
-    apple:"http://newsapi.org/v2/everything?q=apple&from=2020-09-07&to=2020-09-07&sortBy=popularity&apiKey=01ff551c8604416ca22e94875f6265f1",
-    tech:"http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=01ff551c8604416ca22e94875f6265f1",
-    wallstreet:"http://newsapi.org/v2/everything?domains=wsj.com&apiKey=01ff551c8604416ca22e94875f6265f1"
+    bitcoin:"https://newsapi.org/v2/everything?q=bitcoin&from=2020-08-09&sortBy=publishedAt&apiKey=01ff551c8604416ca22e94875f6265f1",
+    busines:"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=01ff551c8604416ca22e94875f6265f1",
+    apple:"https://newsapi.org/v2/everything?q=apple&from=2020-09-07&to=2020-09-07&sortBy=popularity&apiKey=01ff551c8604416ca22e94875f6265f1",
+    tech:"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=01ff551c8604416ca22e94875f6265f1",
+    wallstreet:"https://newsapi.org/v2/everything?domains=wsj.com&apiKey=01ff551c8604416ca22e94875f6265f1"
 
   }
 
